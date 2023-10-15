9e506b2be52613531fd9e45037a34b477a41dcf1
Anushka Mantri <85311996+anushkamantri@users.noreply.github.com>
2023-10-15 13:33:30 -0400

    Placeholder home page

---
+++ src/components/home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>Travel__</h1>
+      </header>
+      <main>
+        <p>Book your next adventure now!</p>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
